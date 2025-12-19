08d84e25b6347a55c6e0cf74e41befd255e4dd0d
Ramadhanul <danuldanul22@gmail.com>
2025-12-19 16:12:05 +0700

    Responsive_Navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -75,11 +75,17 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('documents.index')" :active="request()->routeIs('documents.*')">
+                 {{ __('Documents') }}
             </x-responsive-nav-link>
         </div>
-
+        @if(auth()->user()->role === 'user')
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('chatbot.index')" :active="request()->routeIs('chatbot.*')">
+                 {{ __('Chatbot') }}
+            </x-responsive-nav-link>
+        </div>
+        @endif
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
